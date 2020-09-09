a525b6c143c05b340d0d615177bcdb440106b2b4
Keith O'Neill <keithoneill@keithmacbookpro.local>
2020-09-08 22:10:53 -0400

    changed api to bing news api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import SearchInput from './components/search/SearchInput';
 import ArticleLeft from './components/article/ArticleLeft';
-import ArticleRight from './components/article/ArticleRight';
+//import ArticleRight from './components/article/ArticleRight';
 
 
 
@@ -25,112 +25,96 @@ const handleSubmit = event => {
 
 function getNews() {
   //e.preventDefault();
-  async function searchArticles() {
-    var url = 'http://newsapi.org/v2/everything?' +
-    'q=' +
-    value +
-    '&' +
-    'from=2020-08-10&' +
-    'sources?language=en&' +
-    'apiKey=d071bbbfe8e04f14bee177be536fde78';
-    const response = await fetch(url);
-    if(response.ok){
-        var req = new Request(url);
-        fetch(req)
-        .then(response => response.json())
-        .then(response => response.articles.map(article => (
-            {
-                source: `${article.source.name}`,
-                author: `${article.author}`,
-                title: `${article.title}`,
-                description: `${article.description}`,
-                image: `${article.urlToImage}`,
-                date: `${article.publishedAt}`,
-                content: `${article.content}`,
-                url: `${article.url}`,
-                lean: `${article.source.id}`
-            }
-        )))
-        .then(article => setArticles(article))
-        .catch (error => console.log(error))
+  fetch(`https://bing-news-search1.p.rapidapi.com/news/search?count=100&freshness=Day&textFormat=Raw&safeSearch=Off&q=${value}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+      "x-rapidapi-key": "d133bebc70mshf921f89b5fca90dp119555jsn402fe065fe99",
+      "x-bingapis-sdk": "true"
     }
-  }
-  searchArticles();
+    })
+    .then(response => response.json())
+    //.then(data => console.log(data))
+    .then(response => response.value.map(article => (
+        {
+            source: `${article.provider[0].name}`,
+            title: `${article.name}`,
+            description: `${article.description}`,
+            image: `${article.image.thumbnail.contentUrl}`,
+            date: `${article.datePublished}`,
+            content: `${article.url}`,
+            url: `${article.url}`
+        }
+    )))
+    .then(article => setArticles(article))
+    .catch (error => console.log(error))
 }
 
-function leaningLeft(source){
-  switch (source) {
-    case 'abc-news':
-    case 'abc-news-au':
-    case 'al-jazeera-english':
-    case 'ars-technica':
-    case 'axios':
-    case 'bloomberg':
-    case 'business-insider':
-    case 'business-insider-uk':
-    case 'buzzfeed':
-    case 'cbc-news':
-    case 'cbs-news':
-    case 'cnn':
-    case 'engadget':
-    case 'entertainment-weekly':
-    case 'google-news':
-    case 'google-news-au':
-    case 'google-news-ca':
-    case 'google-news-in':
-    case 'google-news-uk':
-    case 'independent':
-    case 'mashable':
-    case 'msnbc':
-    case 'nbc-news':
-    case 'newsweek':
-    case 'new-york-magazine':
-    case 'politico':
-    case 'polygon':
-    case 'reddit-r-all':
-    case 'rte':
-    case 'the-hindu':
-    case 'the-huffington-post':
-    case 'the-irish-times':
-    case 'the-next-web':
-    case 'the-verge':
-    case 'the-washington-post':
-    case 'time':
-    case 'vice-news':
-      return true;
-    case 'australian-financial-review':
-    case 'breitbart-news':
-    case 'financial-post':
-    case 'fortune':
-    case 'fox-news':
-    case 'fox-sports':
-    case 'national-review':
-    case 'news-com-au':
-    case 'the-american-conservative':
-    case 'the-globe-and-mail':
-    case 'the-times-of-india':
-    case 'the-washington-times':
-      return false;
-    default:
-      return null;
-  }
-}
+
+// function leaningLeft(source){
+//   switch (source) {
+//     case 'abc-news':
+//     case 'abc-news-au':
+//     case 'al-jazeera-english':
+//     case 'ars-technica':
+//     case 'axios':
+//     case 'bloomberg':
+//     case 'business-insider':
+//     case 'business-insider-uk':
+//     case 'buzzfeed':
+//     case 'cbc-news':
+//     case 'cbs-news':
+//     case 'cnn':
+//     case 'engadget':
+//     case 'entertainment-weekly':
+//     case 'google-news':
+//     case 'google-news-au':
+//     case 'google-news-ca':
+//     case 'google-news-in':
+//     case 'google-news-uk':
+//     case 'independent':
+//     case 'mashable':
+//     case 'msnbc':
+//     case 'nbc-news':
+//     case 'newsweek':
+//     case 'new-york-magazine':
+//     case 'politico':
+//     case 'polygon':
+//     case 'reddit-r-all':
+//     case 'rte':
+//     case 'the-hindu':
+//     case 'the-huffington-post':
+//     case 'the-irish-times':
+//     case 'the-next-web':
+//     case 'the-verge':
+//     case 'the-washington-post':
+//     case 'time':
+//     case 'vice-news':
+//       return true;
+//     case 'australian-financial-review':
+//     case 'breitbart-news':
+//     case 'financial-post':
+//     case 'fortune':
+//     case 'fox-news':
+//     case 'fox-sports':
+//     case 'national-review':
+//     case 'news-com-au':
+//     case 'the-american-conservative':
+//     case 'the-globe-and-mail':
+//     case 'the-times-of-india':
+//     case 'the-washington-times':
+//       return false;
+//     default:
+//       return null;
+//   }
+// }
 
 let news = articles.map((articles, i) => {
   if(articles === ''){
     return "Loading..."
   }
   else{
-    if(leaningLeft(articles.lean) === true ){
-      return <ArticleLeft key={i} source={articles.source} author={articles.author} title={articles.title} description={articles.description} image={articles.image} date={articles.date} content={articles.content} />
-    }
-    else if(leaningLeft(articles.lean) === false){
-      return <ArticleRight key={i} source={articles.source} author={articles.author} title={articles.title} description={articles.description} image={articles.image} date={articles.date} content={articles.content} />
-    }
-    else{
-      return null
-    }
-    
+    return <ArticleLeft key={i} source={articles.source} author={articles.author} title={articles.title} description={articles.description} image={articles.image} date={articles.date} content={articles.content} />
   }
 })
 
