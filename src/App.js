d1879710f6bca8640e886117f31d1b5a3e9ec958
Keith O'Neill <keithoneill@keithmacbookpro.local>
2020-09-09 17:29:27 -0400

    added allisdes data to state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchInput from './components/search/SearchInput';
 import ArticleLeft from './components/article/ArticleLeft';
 //import ArticleRight from './components/article/ArticleRight';
+import allsidesData from './components/data/allsidesData.json'
 
 
 
 function App() {
 const [articles, setArticles] = useState([]);
 const [value, setValue] = useState('');
+const [data, setData] = useState([]);
+
+useEffect(() => {
+  const ratingData = allsidesData.map(ratingData => ({
+    source: `${ratingData.news_source}`,
+    rating: `${ratingData.rating}`
+  }))
+  setData({data: ratingData})
+},[data]);
 
 const handleChange = event => {
   setValue(event.target.value);
@@ -25,7 +35,7 @@ const handleSubmit = event => {
 
 function getImage(image){
   if(typeof image === 'undefined'){
-    return 'https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80'
+    return 'https://images.unsplash.com/photo-1585829365295-ab7cd400c167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80  '
   }
   else{
     return image.thumbnail.contentUrl;
@@ -33,89 +43,41 @@ function getImage(image){
 }
 
 function getNews() {
-  //e.preventDefault();
-  fetch(`https://api.cognitive.microsoft.com/bing/v7.0/news/search?count=100&freshness=Day&textFormat=Raw&safeSearch=Off&q=${value}`, {
-    "method": "GET",
-    "headers": {
-      "Ocp-Apim-Subscription-Key": "fea5f0d614de4784b07bf08c6ac387a6",
-      "Accept": "application/json"
-    }
-    })
-    .then(response => response.json())
-    //.then(data => console.log(data))
-    .then(response => response.value.map(article => (
-        {
-            source: `${article.provider[0].name}`,
-            title: `${article.name}`,
-            description: `${article.description}`,
-            image: getImage(article.image),
-            date: `${article.datePublished}`,
-            content: `${article.url}`,
-            url: `${article.url}`
+  async function fetchNews(){
+    const result = await fetch(`https://api.cognitive.microsoft.com/bing/v7.0/news/search?count=100&freshness=Day&textFormat=Raw&safeSearch=Off&q=${value}`, {
+      "method": "GET",
+      "headers": {
+        "Ocp-Apim-Subscription-Key": "fea5f0d614de4784b07bf08c6ac387a6",
+        "Accept": "application/json",
+      }
+      });
+    if(result.ok){
+      fetch(`https://api.cognitive.microsoft.com/bing/v7.0/news/search?count=100&freshness=Day&textFormat=Raw&safeSearch=Off&q=${value}`, {
+        "method": "GET",
+        "headers": {
+          "Ocp-Apim-Subscription-Key": "fea5f0d614de4784b07bf08c6ac387a6",
+          "Accept": "application/json",
         }
-    )))
-    .then(article => setArticles(article))
-    .catch (error => console.log(error))
+        })
+        .then(response => response.json())
+        .then(response => response.value.map(article => (
+            {
+                source: `${article.provider[0].name}`,
+                title: `${article.name}`,
+                description: `${article.description}`,
+                image: getImage(article.image),
+                date: `${article.datePublished}`,
+                content: `${article.url}`,
+                url: `${article.url}`
+            }
+        )))
+        .then(article => setArticles(article))
+        .catch (error => console.log(error))
+    }
+  }
+  fetchNews();
 }
 
-
-// function leaningLeft(source){
-//   switch (source) {
-//     case 'abc-news':
-//     case 'abc-news-au':
-//     case 'al-jazeera-english':
-//     case 'ars-technica':
-//     case 'axios':
-//     case 'bloomberg':
-//     case 'business-insider':
-//     case 'business-insider-uk':
-//     case 'buzzfeed':
-//     case 'cbc-news':
-//     case 'cbs-news':
-//     case 'cnn':
-//     case 'engadget':
-//     case 'entertainment-weekly':
-//     case 'google-news':
-//     case 'google-news-au':
-//     case 'google-news-ca':
-//     case 'google-news-in':
-//     case 'google-news-uk':
-//     case 'independent':
-//     case 'mashable':
-//     case 'msnbc':
-//     case 'nbc-news':
-//     case 'newsweek':
-//     case 'new-york-magazine':
-//     case 'politico':
-//     case 'polygon':
-//     case 'reddit-r-all':
-//     case 'rte':
-//     case 'the-hindu':
-//     case 'the-huffington-post':
-//     case 'the-irish-times':
-//     case 'the-next-web':
-//     case 'the-verge':
-//     case 'the-washington-post':
-//     case 'time':
-//     case 'vice-news':
-//       return true;
-//     case 'australian-financial-review':
-//     case 'breitbart-news':
-//     case 'financial-post':
-//     case 'fortune':
-//     case 'fox-news':
-//     case 'fox-sports':
-//     case 'national-review':
-//     case 'news-com-au':
-//     case 'the-american-conservative':
-//     case 'the-globe-and-mail':
-//     case 'the-times-of-india':
-//     case 'the-washington-times':
-//       return false;
-//     default:
-//       return null;
-//   }
-// }
 
 let news = articles.map((articles, i) => {
   if(articles === ''){
