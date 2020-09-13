6cf6d5745e8f39fb22876a64dfd09e9436efe2c4
keithoneill <koneill1@student.fullsail.edu>
2020-09-13 19:57:53 -0400

    created header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from '../../images/logo192.png'
+
+function Header() {
+
+    return(
+        <header style={styles.header}>
+            <img src={logo} alt="YourOpinionLogo" style={styles.logo} />
+            <h1 style={styles.h1}>Your Opinion</h1>
+        </header>
+    )
+}
+
+export default Header
+
+const styles={
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        background: '#f1faee',
+        
+    },
+    h1:{
+        fontFamily: 'Cairo, sans-serif',
+        src: 'url("https://fonts.googleapis.com/css2?family=Cairo")',
+        fontSize: '3rem',
+        color: '#060C0E',
+        marginLeft: 'auto',
+        paddingRight: '3.5rem',
+        marginRight: 'auto'
+    },
+    logo:{
+        width: '3.5rem',
+        paddingLeft: '2rem'
+    },
+    ul:{
+        display: 'flex',
+        flexDirection: 'row'
+    }
+}
