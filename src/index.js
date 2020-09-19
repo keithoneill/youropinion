e3afaa3f31242835c3d3df04b73f77db2bfbded6
Keith O'Neill <keithoneill@keithmacbookpro.lan>
2020-09-19 15:17:30 -0400

    created  sass file to override bootstrap variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
