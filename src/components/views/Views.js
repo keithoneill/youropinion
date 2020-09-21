2668d650cabd3a6e00aa06a4834422cf4784436e
Keith O'Neill <keithoneill@keithmacbookpro.lan>
2020-09-20 20:52:25 -0400

    added css for button styling

--- src/components/views/Views.js
+++ src/components/views/Views.js
@@ -6,11 +6,51 @@ import TwoColumnNews from '../../pages/TwoColumnNews';
 import ThreeColumnNews from '../../pages/ThreeColumnNews';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import './Views.css';
+import { Container, Row , Col} from 'react-bootstrap';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 
 function Routes() {
+    const renderTooltip1 = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+        This will dispay the news results in two columns, a left and a right.
+    </Tooltip>
+    );
+    const renderTooltip2 = (props) => (
+        <Tooltip id="button-tooltip" {...props}>
+            This will dispay the news results in three columns, a left, a right, and center.
+        </Tooltip>
+        );
+
     return(
         <MemoryRouter>
+            <Container fluid>
+                <Row>
+                    <Col>
+                    <ButtonToolbar className="custom-btn-toolbar">
+                        
+                        <OverlayTrigger
+                            placement="right"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={renderTooltip1}>
+                            <LinkContainer to="/" >
+                                <Button className="buttons">Two Columns</Button>
+                            </LinkContainer>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                            placement="left"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={renderTooltip2}>
+                            <LinkContainer to="/ThreeColumnNews">
+                                <Button className="buttons">Three Columns</Button>
+                            </LinkContainer>
+                        </OverlayTrigger>
+                        
+                    </ButtonToolbar>
+                    </Col>
+                </Row>
+            </Container>
                 <Switch>
                     <Route path="/ThreeColumnNews">
                         <ThreeColumnNews />
@@ -19,14 +59,7 @@ function Routes() {
                         <TwoColumnNews />
                     </Route>
                 </Switch>
-                <ButtonToolbar className="buttongroup">
-                    <LinkContainer to="/" >
-                        <Button className="buttonone buttons">Two Columns</Button>
-                    </LinkContainer>
-                    <LinkContainer to="/ThreeColumnNews">
-                        <Button className="buttontwo buttons">Three Columns</Button>
-                    </LinkContainer>
-                </ButtonToolbar>
+                
         </MemoryRouter>
     )
 }
