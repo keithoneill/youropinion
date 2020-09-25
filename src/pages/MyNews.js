25cbbc3e2f0d0933d9d007b47f3c93fe6c57baa0
keithoneill <koneill1@student.fullsail.edu>
2020-09-25 00:07:57 -0400

    fixed mapping over state

--- src/pages/MyNews.js
+++ src/pages/MyNews.js
@@ -1,29 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import SearchInput from '../components/search/SearchInput';
+import SearchFor from '../components/searchFor/SearchFor';
+import mediaBiasFactCheckData from '../components/data/mediaBiasFactCheckData.json';
+import NewsSource from '../components/source/NewsSource';
+import Loading from '../components/loading/Loading';
 
 function MyNews(){
+const [source, setSource] = useState([]);
+const [value, setValue] = useState('');
+const [searchFor, setSearch] = useState('');
+
+const handleChange = event => {
+    setValue(event.target.value);
+    };
+    
+    const handleSubmit = event => {
+    if (value) {
+        getNewsSource(value);
+    }
+    setSearch(value);
+    setValue('');
+    event.preventDefault();
+    };
+
+function getNewsSource(name){
+    let outlet = name.toLowerCase();
+    for (const [source] of Object.entries(mediaBiasFactCheckData)) {
+        var newsOutlet = mediaBiasFactCheckData[source];
+        if (newsOutlet.n.toLowerCase().includes(outlet)){
+            let sources = Array.from(Object.entries(newsOutlet));
+            console.log(sources);
+        let results = sources.map(source =>(
+            {              
+                name: source.n,
+                leaning: source.b,
+                url: source.h,
+                factualReporting: source.r,
+                mediaBiasUrl: source.u
+            }
+        ))
+        setSource(results);
+        }
+    }
+}
+let sourceData = source.map((source, i) => {
+    if(source === ''){
+    return <Loading/>
+    }
+    else{
+        if(source !== ''){
+        return <NewsSource key={i} source={source.name} bias={source.leaning} url={source.url} biasurl={source.mediaBiasUrl} reporting={source.factualReporting}/>
+        }
+        else{
+        return null;
+    }
+    }
+})
+
+
     return(
         <Container>
             <Row>
                 <Col style={styles.col}>
-                <h1>Your Opinion</h1>
-                <h2>Where Does Your News Lean?</h2>
-                <p>With today's current technology, news is more accessible than ever. 
-                    From dedicated news outlets to social media, people have access to more news than ever. 
-                    But, how reliable is the news? Few people ask that question. Most people tend to get their news from one source. 
-                    People tend to stay with what they are most comfortable with. As we have seen in recent years, this can be a danger. 
-                    If a majority of individuals get their news from one source, the news outlets have great power. 
-                    They can influence their user base now more than ever. As Thomas Jefferson outlined, it is important to have an informed electorate.</p>
-                <p>Your Opinion is web application that is 
-                    intended to allow the user an easier way to develop their own opinion. 
-                    Instead of having their opinion swayed by the news outlet they mostly use, Your Opinion will display 
-                    news from multiple outlets at once. Not only that, Your Opinion will divide the news outlets between 
-                    left and right leaning companies. The information will be displayed simply in a left and right column. 
-                    This will allow the user to clearly see how the information is being presented to them and allow them to form their own opinion.</p>
-                <p>Your Opinion will gather any searched topic from thousands of news sources through the internet. 
-                    Then, each related article is sorted into columns based on the sources biased rating from <a href="https://mediabiasfactcheck.com/">Media Bias Fact Check</a>
-                </p>
+                    <h1>Your Opinion</h1>
+                    <h2>Where Does Your News Lean?</h2>
+                    <p>Want to know where your news source leans? Just search it below</p>
+                    <SearchInput handleChange={handleChange} handleSubmit={handleSubmit} placeholder="Search For A News Source"/>
                 </Col>
+                <Row>
+                    <Col>
+                        <SearchFor searchFor={searchFor} hasValue={searchFor} />
+                        {sourceData}
+                    </Col>
+                </Row>
             </Row>
         </Container>
     )
