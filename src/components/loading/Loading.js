eb8074a411b03d889ef8a89b8156f6277088a771
Keith O'Neill <keithoneill@keithmacbookpro.lan>
2020-09-27 18:39:24 -0400

    added props for showing loading

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -4,12 +4,16 @@ import Spinner from 'react-bootstrap/Spinner';
 
 
 function Loading(props){
+    const hasValue = props.hasValue;
+    if(hasValue !== ''){
     return(
         <Spinner animation="border" role="status">
             <span className="sr-only">Loading...</span>
         </Spinner>
-        
-    )
+    )}
+    else{
+        return null;
+    }
 }
 
 export default Loading
