8bd16c3ef32b8af7f55324222f8addcff14b8a56
Keith O'Neill <keithoneill@keithmacbookpro.lan>
2020-09-27 19:18:59 -0400

    Updates

---
+++ precache-manifest.34a8ed8789708c11f942a307a89dc3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2b5c206f3bec7fc58120454d9ebc51",
+    "url": "/keithoneill/youropinion/index.html"
+  },
+  {
+    "revision": "82031a1699c9e039ad58",
+    "url": "/keithoneill/youropinion/static/css/main.1a67da09.chunk.css"
+  },
+  {
+    "revision": "5db1bdd06abb6cd8479a",
+    "url": "/keithoneill/youropinion/static/js/2.58b1081d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/keithoneill/youropinion/static/js/2.58b1081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82031a1699c9e039ad58",
+    "url": "/keithoneill/youropinion/static/js/main.b7251f92.chunk.js"
+  },
+  {
+    "revision": "e04797c2474d33effb40",
+    "url": "/keithoneill/youropinion/static/js/runtime-main.709af2e9.js"
+  },
+  {
+    "revision": "e26dec280f73a9b6665d4f3d457024d6",
+    "url": "/keithoneill/youropinion/static/media/logo192.e26dec28.png"
+  }
+]);
