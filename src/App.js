495dd4de31ddf17237d313287366b78b1c1efeab
Keith O'Neill <keithoneill@keithmacbookpro.lan>
2020-09-27 19:32:56 -0400

    changed router to hash router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 
 
